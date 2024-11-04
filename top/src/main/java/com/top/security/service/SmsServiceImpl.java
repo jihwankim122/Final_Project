aa91094213bc20d0e5ccc04bc02ed4880a99ec98
YuJin <vwx650@gmail.com>
2024-11-04 14:27:45 +0900

    휴대폰 인증 추가

---
+++ top/src/main/java/com/top/security/service/SmsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.top.security.service;
+
+import com.top.security.dto.SmsRequestDto;
+import com.top.security.util.SmsCertificationUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class SmsServiceImpl implements SmsService {
+
+    private final SmsCertificationUtil smsCertificationUtil;
+
+    @Autowired
+    public SmsServiceImpl(SmsCertificationUtil smsCertificationUtil) {
+        this.smsCertificationUtil = smsCertificationUtil;
+    }
+
+    @Override
+    public String sendSms(SmsRequestDto smsRequestDto) {
+        String certificationCode = String.format("%06d", new Random().nextInt(1000000)); // 6자리 인증 코드
+        smsCertificationUtil.sendSMS(smsRequestDto.getPhoneNum(), certificationCode);
+        return certificationCode;
+    }
+}
+
